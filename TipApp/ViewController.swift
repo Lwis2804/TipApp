90c18357baaeb3ed997693d1ee7d1a78ab180afa
LUIS <luis.enrique.enc@gmail.com>
2024-01-02 10:14:23 -0600

    Initial Commit

---
+++ TipApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipApp
+//
+//  Created by LUIS GONZALEZ on 02/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
