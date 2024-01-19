86fa1d70ff8b43ee31ca902c569b3088c492cf37
LUIS <luis.enrique.enc@gmail.com>
2024-01-18 18:18:06 -0600

    [ADD] vista para dividir entre personas lista

--- TipApp/Views/TipOnePerPerson/TipOnePersonViewController.swift
+++ TipApp/Views/TipOnePerPerson/TipOnePersonViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class TipOnePersonViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
